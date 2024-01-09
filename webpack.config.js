6347c0b637b4c5da669da39c5a60e5a6fc50d2a0
Mihai Razvan Tudorache <mtudorache@lenovo.com>
2024-01-09 11:37:07 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  name: "mfe1",
+
+  exposes: {
+    "./Card": "./src/app/card/card.component.ts",
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
